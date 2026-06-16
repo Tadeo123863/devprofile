0d1ebfe4fff4aad6b111f87b12c6d32aa385b797
Tadeo123863 <tadeoandradesustaita@gmail.com>
2026-06-15 21:27:46 -0600

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,11 +5,10 @@ const AboutPage = () => {
     <div className="card">
       <h1>Acerca del proyecto</h1>
       <p><strong>DevProfile</strong> - Proyecto final de Tecnologías Web / React</p>
-      <p>Desarrollado por: [Nombres del equipo]</p>
+      <p>Desarrollado por: [Tadeo Andrade Sustaita]</p>
       <p>Este proyecto cumple con todos los requisitos: CRUD completo, persistencia, modo oscuro, gráfica de habilidades, generación de PDF, validaciones y rutas.</p>
-      <p>Fecha de entrega: 10 de junio</p>
-      <p>Repositorio: <a href="#">[link de GitHub]</a></p>
-      <p>Deploy: <a href="#">[link del deploy]</a></p>
+      <p>Fecha de entrega: 15 de junio</p>
+      <p>Repositorio: <a href="#">[https://github.com/Tadeo123863/devprofile]</a></p>
     </div>
   );
 };
